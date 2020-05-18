808910d07beb4853641396ac7ec7b445348beaa0
ouksal <ozgur.uksal@gmail.com>
2020-05-18 01:30:21 -0400

    separated frontend and backend workflow

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	//})
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	http.HandleFunc("/selamun", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/selamun-aleykum", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Selamun Aleykum")
 	})
 
